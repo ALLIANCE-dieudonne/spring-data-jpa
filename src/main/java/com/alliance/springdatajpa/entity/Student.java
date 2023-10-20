54b99c58be216f94554f3d2f2f2563619b4af90e
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-10-20 18:48:54 +0200

    different jpa annotations

--- src/main/java/com/alliance/springdatajpa/entity/Student.java
+++ src/main/java/com/alliance/springdatajpa/entity/Student.java
@@ -1,7 +1,10 @@
 package com.alliance.springdatajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,14 +15,45 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+  name = "tbl_student",
+  uniqueConstraints = @UniqueConstraint(
+    name = "unique_email",
+    columnNames = "email_address"
+  )
+
+)
 public class Student {
   @Id
+  @SequenceGenerator(
+    name = "student_sequence",
+    sequenceName = "student_sequence",
+    allocationSize = 1
+  )
+  @GeneratedValue(
+    strategy = GenerationType.SEQUENCE,
+    generator = "student_sequence"
+  )
   private Long studentId;
+
+
+  @NotNull(message = "The name should not be empty!")
+  @Min(10)
+  @Max(25)
   private String fName;
+
+  @NotNull(message = "The name should not be empty!")
+  @Min(10)
+  @Max(25)
   private String lName;
+
+  @Column(name = "email_address", nullable = false)
   private String emailId;
   private String guardianName;
+
+  @Email(message = "Enter a valid email")
   private String guardianEmail;
+
   private String guardianMobile;
 
 
