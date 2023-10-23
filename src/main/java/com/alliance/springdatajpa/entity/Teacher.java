a9cf08432038358e62e70d330caab9969e79c185
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-10-23 20:19:31 +0200

    one to many relation

---
+++ src/main/java/com/alliance/springdatajpa/entity/Teacher.java
@@ -0,0 +1,42 @@
+package com.alliance.springdatajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Teacher {
+  @Id
+  @SequenceGenerator(
+    name = "teacher_sequence",
+    sequenceName = "teacher_sequence",
+    allocationSize = 1
+  )
+
+  @GeneratedValue(
+    strategy = GenerationType.SEQUENCE,
+    generator = "teacher_sequence"
+  )
+  private Long teacherId;
+  private String fName;
+  private String lName;
+
+  @OneToMany(
+    cascade = CascadeType.ALL,
+    fetch = FetchType.LAZY
+  )
+  @JoinColumn(
+    name = "teacher_id",
+    referencedColumnName = "teacherId"
+  )
+  private List<Course> courses;
+}
