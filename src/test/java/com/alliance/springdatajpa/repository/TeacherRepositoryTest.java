db90015d92e7bd616bfaac60a490a9c92a68d815
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-10-23 20:34:34 +0200

    done with one to many mapping

---
+++ src/test/java/com/alliance/springdatajpa/repository/TeacherRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.alliance.springdatajpa.repository;
+
+import com.alliance.springdatajpa.entity.Course;
+import com.alliance.springdatajpa.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+
+  @Autowired
+  TeacherRepository teacherRepository;
+
+  @Test
+  public void saveTeacher(){
+
+    Course course = Course.builder()
+      .title("chem")
+      .credit(1)
+      .build();
+    Course course2 = Course.builder()
+      .title("his")
+      .credit(1)
+      .build();
+    Teacher teacher = Teacher.builder()
+      .fName("fils")
+      .lName("dios")
+      .courses(List.of(course, course2))
+      .build();
+
+    teacherRepository.save(teacher);
+  }
+
+}
